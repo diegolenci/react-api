d825bce552e84ff77e30eb2cd3d1ff83cec83191
diegolenci <lencidiego0@gmail.com>
2025-02-26 16:11:50 +0100

    add: chiama api fetch

--- react-api/src/App.jsx
+++ react-api/src/App.jsx
@@ -5,6 +5,16 @@ function App() {
   //impostare array vuoto per i post
   const [posts, setPosts] = useState([])
   
+  //chiamata api con metodo fetch
+  const fetchPosts = () => {
+    fetch('http://localhost:3000/api/posts')
+    .then( (res) => res.json() )
+    .then( setPosts )
+    .catch( err => console.error( err ))
+  }
+
+  useEffect( fetchPosts, [] )
+  
   return (
     <>
       
